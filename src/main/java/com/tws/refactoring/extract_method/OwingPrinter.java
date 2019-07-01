ab8131d003f9fc32b200774c80eaa9aab28b0682
yaeli <yaeli@thoughtworks.com>
2019-07-01 19:39:20 +0800

    [yae][refactoring by extracting method]

--- src/main/java/com/tws/refactoring/extract_method/OwingPrinter.java
+++ src/main/java/com/tws/refactoring/extract_method/OwingPrinter.java
@@ -7,23 +7,36 @@ import java.util.Vector;
 
 public class OwingPrinter {
     void printOwing(String name, List<Order> orders) {
-        Iterator<Order> elements = orders.iterator();
-        double outstanding = 0.0;
 
         // print banner
-        System.out.println ("*****************************");
-        System.out.println ("****** Customer totals ******");
-        System.out.println ("*****************************");
+        printBanner();
 
         // print owings
+        double outstanding = getOutstanding(orders);
+
+        // print details
+        printDetails(name, outstanding);
+    }
+
+    private void printDetails(String name, double outstanding) {
+        System.out.println("name: " + name);
+        System.out.println("amount: " + outstanding);
+    }
+
+    private double getOutstanding(List<Order> orders) {
+        Iterator<Order> elements = orders.iterator();
+        double outstanding = 0.0;
         while (elements.hasNext()) {
             Order each = (Order) elements.next();
             outstanding += each.getAmount();
         }
+        return outstanding;
+    }
 
-        // print details
-        System.out.println("name: " + name);
-        System.out.println("amount: " + outstanding);
+    private void printBanner() {
+        System.out.println ("*****************************");
+        System.out.println ("****** Customer totals ******");
+        System.out.println ("*****************************");
     }
 }
 
